11c173a11d81e15335fb872ae72c00f93829dfd1
Emanoel Delfino <emanoeldelfno@gmail.com>
2021-08-19 10:57:38 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,76 @@
 const date1Elem = document.querySelector("#date1");
 const date2Elem = document.querySelector("#date2");
-const convertUnit = document.querySelector("#time-unit");
+const convertUnitElem = document.querySelector("#time-unit");
+const spanConverted = document.querySelector("#converted");
 
-const date1 = New Date(date1Elem.value);
-const date2 = New Date(date2Elem.value);
+function capitalizeFirstLetter(string) {
+	return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
-const diffMs = date1.getTime() - date2.getTime();
+[date1Elem, date2Elem, convertUnitElem].forEach(elem => {
+	elem.addEventListener('change', () => {
+		const date1 = new Date(date1Elem.value);
+		const date2 = new Date(date2Elem.value);
+
+		if (date1Elem.value && date2Elem.value) {
+			const convertUnit = convertUnitElem.value.toLowerCase();
+			const diffMs = Math.abs(date1.getTime() - date2.getTime());
+			const diffSecs = convert2Secs.milliseconds(diffMs);
+			console.log(convertUnit);
+			const diffUnit = convertSecs2[convertUnit](diffSecs);
+			spanConverted.innerText = `Difference of ${diffUnit} ${convertUnit}.`;
+			const yearsMonthsWeeksDays = getYearsMonthsWeeksDays(diffSecs);
+
+			for (const [key, value] of Object.entries(yearsMonthsWeeksDays)) {
+				const unitElem = document.querySelector(`#${key}`);
+				unitElem.innerText = `${capitalizeFirstLetter(key)}: ${value}`;
+			}
+		}
+	})
+});
+
+function getYearsMonthsWeeksDays(seconds) {
+	const units = ["years", "months", "weeks", "days"];
+	let converted = {};
+	let remainingSecs = seconds;
+	for (let unit of units) {
+		converted[unit] = Math.floor(remainingSecs / conversionSecs[unit]);
+		remainingSecs = remainingSecs % conversionSecs[unit];
+	}
+
+	return converted;
+}
+
+const conversionSecs = {
+	picoseconds: 1 / 10 ** 12,
+	nanoseconds: 1 / 10 ** 9,
+	microseconds: 1 / 10 ** 6,
+	milliseconds: 1 / 10 ** 3,
+	seconds: 1,
+	minutes: 60,
+	hours: 60 ** 2,
+	days: (60 ** 2 * 24),
+	weeks: (60 ** 2 * 24 * 7),
+	months: (60 ** 2 * 24 * 30),
+	years: (60 ** 2 * 24 * 365),
+	decades: (60 ** 2 * 24 * 365 * 10),
+	centuries: (60 ** 2 * 24 * 365 * 100),
+}
 
 const convertSecs2 = {
 	picoseconds: secs => secs * 10 ** 12,
 	nanoseconds: secs => secs * 10 ** 9,
 	microseconds: secs => secs * 10 ** 6,
 	milliseconds: secs => secs * 10 ** 3,
-	minutes: secs => secs / 60
-	hours: secs => secs / 60 ** 2
-	days: secs => secs / (60 ** 2 * 24)
-	weeks: secs => secs / (60 ** 2 * 24 * 7)
-	months: secs => secs / (60 ** 2 * 24 * 30)
-	years: secs => secs / (60 ** 2 * 24 * 365)
-	decades: secs => secs / (60 ** 2 * 24 * 365 * 10)
-	centuries: secs => secs / (60 ** 2 * 24 * 365 * 100)
+	seconds: secs => secs,
+	minutes: secs => secs / 60,
+	hours: secs => secs / 60 ** 2,
+	days: secs => secs / (60 ** 2 * 24),
+	weeks: secs => secs / (60 ** 2 * 24 * 7),
+	months: secs => secs / (60 ** 2 * 24 * 30),
+	years: secs => secs / (60 ** 2 * 24 * 365),
+	decades: secs => secs / (60 ** 2 * 24 * 365 * 10),
+	centuries: secs => secs / (60 ** 2 * 24 * 365 * 100),
 }
 
 const convert2Secs = {
@@ -27,12 +78,13 @@ const convert2Secs = {
 	nanoseconds: unit => unit / 10 ** 9,
 	microseconds: unit => unit / 10 ** 6,
 	milliseconds: unit => unit / 10 ** 3,
-	minutes: unit => unit * 60
-	hours: secs => unit * 60 ** 2
-	days: secs => unit * (60 ** 2 * 24)
-	weeks: secs => unit * (60 ** 2 * 24 * 7)
-	months: secs => unit * (60 ** 2 * 24 * 30)
-	years: secs => unit * (60 ** 2 * 24 * 365)
-	decades: secs => unit * (60 ** 2 * 24 * 365 * 10)
-	centuries: secs => unit * (60 ** 2 * 24 * 365 * 100)
+	seconds: secs => secs,
+	minutes: unit => unit * 60,
+	hours: unit => unit * 60 ** 2,
+	days: unit => unit * (60 ** 2 * 24),
+	weeks: unit => unit * (60 ** 2 * 24 * 7),
+	months: unit => unit * (60 ** 2 * 24 * 30),
+	years: unit => unit * (60 ** 2 * 24 * 365),
+	decades: unit => unit * (60 ** 2 * 24 * 365 * 10),
+	centuries: unit => unit * (60 ** 2 * 24 * 365 * 100),
 }
